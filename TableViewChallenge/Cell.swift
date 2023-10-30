a5db51d0eb8a0a4c195200cea35f80cfe8d45c47
Stanislav <stazilastas@gmail.com>
2023-10-30 09:59:41 -0500

    Table View Challenge 1.0.0

---
+++ TableViewChallenge/Cell.swift
@@ -0,0 +1,12 @@
+//
+//  Cell.swift
+//  TableViewChallenge
+//
+//  Created by STANISLAV STAJILA on 10/30/23.
+//
+
+import Foundation
+
+public class Cell: {
+    
+}
--- TableViewChallenge/ViewController.swift
+++ TableViewChallenge/ViewController.swift
@@ -7,12 +7,36 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+var names: [String] = ["Stanislav", "Brian", "Gabi"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        tableView.dataSource = self
+        tableView.delegate = self
     }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var cell = tableView.dequeueReusableCell(withIdentifier: "myCell")!
+        
+        cell.textLabel?.text = "\(names[indexPath.row])"
+        
+        return cell
+        
+    }
+    
+    
 
 
 }
